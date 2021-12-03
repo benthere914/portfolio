dcc045efbb12ec97cd7c79868bc3cf55c0203505
Benjamin Rose <benthere914@gmail.com>
2021-12-03 12:55:58 -0600

    made basic home page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import Header from './components/header';
 import Footer from './components/footer';
+import Home from './components/home';
 
 function App() {
 	return (
 		<BrowserRouter>
             <Header/>
 			<Switch>
-                <Route path='/'>
-
+                <Route path='/' exact={true}>
+                    <Home/>
                 </Route>
                 <Route path='/about'>
 
---
+++ src/components/home/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+const Home = () => {
+    return (
+        <>
+            <div className='homePage'>
+                <h2>Benjamin Rose</h2>
+                <img src='https://ca.slack-edge.com/T03GU501J-U025530QL2Y-2ef5d624b3d3-512'></img>
+                <h2>Software Engineer</h2>
+            </div>
+        </>
+    )
+}
+
+export default Home
