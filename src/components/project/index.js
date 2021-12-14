5c3e20390fa96692d146919525623b1e78096a0b
Benjamin Rose <benthere914@gmail.com>
2021-12-14 15:27:46 -0600

    redirects to page

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -3,19 +3,24 @@ import { projectData } from './projects'
 import { useParams } from 'react-router'
 import { useEffect, useState } from 'react'
 import { Carousel } from 'react-responsive-carousel'
+import { useHistory } from 'react-router-dom'
 const Project = () => {
-    const params = useParams()
+    const params = useParams();
+    const history = useHistory();
     const [data, setData] = useState({})
     useEffect(() => {
         setData(projectData[params.projectName])
     }, [params])
+    const visitPage=(data) => {
+        window.open(data?.url)
+    }
     return (
         <>
         <div className='projectPage'>
             <div>
-                <h2 className='siteName'>{data?.name}</h2>
+                <h2 onClick={() => window.open(data?.url)} className='siteName'>{data?.name}</h2>
                 <div className='screenshot'>
-                    <img alt='website screenshot' src={data?.image}></img>
+                    <img onClick={() => window.open(data?.url)} alt='website screenshot' src={data?.image}></img>
                 </div>
                 <h3>Created by: {data?.creators?.join(', ')}</h3>
             </div>
