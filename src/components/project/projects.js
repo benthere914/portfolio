fd467edda73c9cedacd10461e8ebd0cf0041753c
Benjamin Rose <benthere914@gmail.com>
2021-12-14 15:22:36 -0600

    updated projectData to include url of project

--- src/components/project/projects.js
+++ src/components/project/projects.js
@@ -1,6 +1,7 @@
 export const projectData = {
     Acquire: {
         name: 'Acquire',
+        url: 'https://acquire-aa.herokuapp.com/',
         image: 'https://user-images.githubusercontent.com/8016326/142956978-3baea7af-7e2f-4235-84e6-c9caac3acc92.png',
         creators: ['Benjamin Rose'],
         technologies: [
@@ -11,6 +12,7 @@ export const projectData = {
     },
     Auto_BnB: {
         name: 'Auto-BnB',
+        url: 'https://auto-bnb.herokuapp.com/',
         image: 'https://user-images.githubusercontent.com/8016326/144485593-94203d73-a28e-4cbd-b513-3bad523502c4.png',
         creators: ['Benjamin Rose'],
         technologies: [
@@ -21,6 +23,7 @@ export const projectData = {
     },
     Discuss: {
         name: 'Discuss',
+        url: 'https://discuss-aa.herokuapp.com/',
         image: 'https://camo.githubusercontent.com/0fe6b6a1be0e560ed21011d3c68dc8d94a80f2e35e426b13f2ca3ef36965c2ca/68747470733a2f2f696d6775722e636f6d2f315730453547532e706e67',
         creators: ['Benjamin Rose', 'Kiara Mendaros', 'Jack Percival', 'Valeria Reynoso'],
         technologies: [
@@ -31,6 +34,7 @@ export const projectData = {
     },
     Equilibrium: {
         name: 'Equilibrium',
+        url: 'https://equilibrium.herokuapp.com/',
         image: 'https://user-images.githubusercontent.com/8016326/144483962-c0f5a459-537a-4025-9ff6-8a677377e258.png',
         creators: ['Benjamin Rose', 'Nick Frase', 'Parker Bolick'],
         technologies: [
